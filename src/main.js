e651f9796e6a351cffd66e0d6db436e9e69eb1ca
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-04-28 00:26:48 +0300

    added change of themes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
-// TODO: Add js
+const theme = localStorage.getItem('data-theme');
+const form = document.querySelector('.form');
+const changeTheme = (color) => {
+  document.documentElement.setAttribute('data-theme', color);
+  localStorage.setItem('data-theme', color);
+};
+
+changeTheme(theme);
+
+form.addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const selectedColor = clickEvent.target.value;
+  const selectedClassName = clickEvent.target.className;
+  const selectedInputId = clickEvent.target.htmlFor;
+  const selectInput = clickEvent.target.closest('.form__radio');
+  const selectedInput = document.getElementById(selectedInputId);
+
+  if (selectInput && selectedInputId) {
+    selectedInput.checked = true;
+  }
+
+  switch (selectedColor || selectedClassName) {
+    case 'lightTheme':
+      changeTheme('light');
+      break;
+
+    case 'darkTheme':
+      changeTheme('dark');
+      break;
+
+    case 'grayTheme':
+      changeTheme('gray');
+      break;
+
+    default:
+      break;
+  }
+});
